66381b042eeff05c98464270e7e25ec00c99a383
David Killian <dpkillian@icloud.com>
2018-01-10 14:35:18 -0500

    routes are working, need to work on logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -35,14 +35,15 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-    if (tableData.length < 5) {
-      tableData.push(req.body);
-      res.json(true);
-    }
-    else {
-      waitListData.push(req.body);
-      res.json(false);
-    }
+
+    var userResponse = req.body;
+    var userScores = userResponse.scores;
+
+    console.log('userInput = ' + JSON.stringify(userResponse));
+    friendsData.push(userResponse);
+    console.log(friendsData);
+
+    
   });
 
   // ---------------------------------------------------------------------------
