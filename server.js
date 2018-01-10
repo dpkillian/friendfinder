b16d40a7b980ce836d229270ef55948f3851e370
David Killian <dpkillian@icloud.com>
2018-01-10 11:08:01 -0500

    All is working except core survey logic

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var app = express();
 var PORT = process.env.PORT || 3030; 
 
 // Sets up the Express app to handle data parsing
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // ================================================================================
@@ -29,8 +29,8 @@ app.use(bodyParser.json());
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 // =============================================================================
 // LISTENER
