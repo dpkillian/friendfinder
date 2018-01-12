aec6e3dfb678f3dd3fc95b14682444f4bcdcda7c
David Killian <dpkillian@icloud.com>
2018-01-12 13:21:29 -0500

    changed friendsArray, added comments

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,6 +1,6 @@
 // ===============================================================================
 // DATA
-// Below is an initial set of friends data.
+// Below is an initial set of friends data (name, photo link and question scores).
 // ===============================================================================
 
 
@@ -10,15 +10,15 @@ var friendsArray = [
 	"photo": "https://pbs.twimg.com/profile_images/782474226020200448/zDo-gAo0_400x400.jpg",
 	"scores": [
 		"5",
-		"1",
-		"4",
-		"4",
 		"5",
-		"1",
-		"2",
 		"5",
-		"4",
-		"1"
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5",
+		"5"
 	] 
 },
 
@@ -27,15 +27,15 @@ var friendsArray = [
 	"photo": "https://mt-cdn-prod.azureedge.net/mediacache/7/a/5/3/e/6/7a53e6c1e9ab6ba3b16ed1ac72193bff514e3a45.jpg",
 	"scores": [
 		"4",
-		"2",
-		"5",
-		"1",
-		"3",
-		"2",
-		"2",
-		"1",
-		"3",
-		"2"
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4"
 	]
 },
 
@@ -43,16 +43,16 @@ var friendsArray = [
 	"name": "Barack Obama",
 	"photo": "https://pbs.twimg.com/profile_images/822547732376207360/5g0FC8XX.jpg",
 	"scores": [
-		"5",
 		"2",
 		"2",
 		"2",
-		"4",
-		"1",
-		"3",
 		"2",
-		"5",
-		"5"
+		"2",
+		"2",
+		"2",
+		"2",
+		"2",
+		"2"
 	]
 },
 
@@ -60,16 +60,16 @@ var friendsArray = [
 	"name": "Bruce Lee",
 	"photo": "https://i.ytimg.com/vi/DBjh61WIL1c/maxresdefault.jpg",
 	"scores": [
-		"3",
-		"3",
-		"4",
-		"2",
-		"2",
 		"1",
-		"3",
-		"2",
-		"2",
-		"3"
+		"1",
+		"1",
+		"1",
+		"1",
+		"1",
+		"1",
+		"1",
+		"1",
+		"1"
 	]
 },
 
@@ -77,16 +77,16 @@ var friendsArray = [
 	"name": "Kim Kardashian",
 	"photo": "http://www.entertainmentsa.co.za/wp-content/uploads/2017/12/kim-kardashian-makeup-promo.jpg",
 	"scores": [
-		"4",
 		"3",
-		"4",
-		"1",
-		"5",
-		"2",
-		"5",
 		"3",
-		"1",
-		"4"
+		"3",
+		"3",
+		"3",
+		"3",
+		"3",
+		"3",
+		"3",
+		"3"
 	]
 },
 
@@ -94,15 +94,15 @@ var friendsArray = [
 	"name": "Elvis Presley",
 	"photo": "https://static.goldderby.com/wp-content/uploads/2017/08/Elvis-Presley-Movies-Ranked-620x360.jpg",
 	"scores": [
-		"4",
-		"3",
-		"4",
-		"1",
 		"5",
-		"2",
 		"5",
-		"3",
-		"1",
+		"5",
+		"5",
+		"5",
+		"4",
+		"4",
+		"4",
+		"4",
 		"4"
 	]
 },
@@ -111,16 +111,16 @@ var friendsArray = [
 	"name": "Tom Cruise",
 	"photo": "https://www.thefamouspeople.com/profiles/images/tom-cruise-1.jpg",
 	"scores": [
-		"5",
+		"4",
+		"4",
+		"4",
+		"4",
+		"4",
 		"3",
 		"3",
-		"1",
 		"3",
-		"2",
-		"5",
-		"2",
-		"2",
-		"4"
+		"3",
+		"3"
 	]
 },
 
@@ -129,15 +129,15 @@ var friendsArray = [
 	"photo": "http://desktop.beiruting.com/content/ResizedImages/768/10000/inside/Angelina-Jolie-promoting--011-150408032038744.jpg",
 	"scores": [
 		"3",
-		"4",
+		"3",
+		"3",
+		"3",
+		"3",
 		"2",
-		"5",
 		"2",
-		"5",
-		"4",
 		"2",
-		"4",
-		"5"
+		"2",
+		"2"
 	]
 },
 
@@ -145,16 +145,16 @@ var friendsArray = [
 	"name": "Dalai Lama",
 	"photo": "http://tibet.net/wp-content/uploads/2011/09/HHDL.png",
 	"scores": [
-		"1",
-		"4",
+		"2",
+		"2",
+		"2",
+		"2",
 		"2",
 		"1",
-		"2",
-		"2",
 		"1",
-		"2",
-		"2",
-		"5"
+		"1",
+		"1",
+		"1"
 	]
 }
 ];
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,10 +12,13 @@ var friendsData = require("../data/friends");
 // ===============================================================================
 
 module.exports = function(app) {
+
+
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/friends... they are shown a JSON of the data in the table)
+  // When a user visits the link /api/friends (statically linked in the footer of
+  // home.html and survey.html) they are shown the data in the friends.js table...
+  // via res.json which returns a JSON object called friendsData which is linked via 'require'
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
@@ -24,15 +27,17 @@ module.exports = function(app) {
 
 
   // API POST Requests
-  // Below code handles when a user submits a form and thus submits data to the server.
-  // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a survey questions...this data is then sent to the server...
-  // Then the server saves the data to the friendsData array (in friends.js))
+  // Below code handles when a user submits the answers to all 10 question. This sends data to the server.
+  // When a user clicks submit the question data is passed thru the "req" variable (a JSON object)
+  // ...and the subsequent statements are executed.  In this code block, the req object (new user name,
+  // photo link and question scores) is parsed.  2 nested loops compare scores of all of the friends in the
+  // friendsArray with the new user/friend.  The totals are compared and a conditional tests to see if
+  // the score comparison is better than the previous friend.  The closest match is saved in the friendsArray
+  // and returned via the res.json method which passes the best match data back to the jquery $.post call
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let the user know
+    // Our "server" will respond to requests and let the user know
     // who is the closest match.  It will do this by comparing the differences in the users' scores.
     // req.body is the "requested" data (user's input from the survey called newFriend)
 
